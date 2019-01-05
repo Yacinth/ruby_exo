abf9b8b0d7a3fe5af3cb1a750f45058038f1cefd
Yacinth <fevriercjjc@gmail.fr>
2019-01-05 02:10:42 +0100

    mise à jour pyramide.rb

--- exo19.rb
+++ exo19.rb
@@ -2,11 +2,12 @@ mail = []
 x = 1
 
 while ( x <= 50 )
-if ( x >= 0 && x=2 && x = 4 && x = 8 )
+y = x % 2
+if ( y == 0 )
 mail << "jean.dupont.0#{x}@email.fr"
 else
-mail << "jean.dupont.#{x}@email.fr"
 end
-x += 1
+x = 2
 end
 puts mail
+
--- pyramide.rb
+++ pyramide.rb
@@ -1,12 +1,24 @@
-mail = []
-x = 1
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print "> "
+user_number = gets.chomp.to_i
+nb = 1
+if user_number > 25
+puts "Entre 1 et 25 petit gourmand"
+print "> "
+user_number = gets.chomp.to_i
+puts "voici la pyramide :"
+while (nb <= user_number && nb < 26 )
+ nb_space = user_number - nb       
+ puts "#{ " " * nb_space + "#" * nb }"
+ nb = nb + 1
+end
 
-while ( x <= 50 )
-if ( x >= 0 && x=2 && x = 4 && x = 8 )
-mail << "jean.dupont.0#{x}@email.fr"
 else
-mail << "jean.dupont.#{x}@email.fr"
+puts "voici la pyramide :"
+while (nb <= user_number && nb < 26 )
+ nb_space = user_number - nb
+ puts "#{ " " * nb_space + "#" * nb }"
+ nb = nb + 1
 end
-x += 1
 end
-puts mail
+
